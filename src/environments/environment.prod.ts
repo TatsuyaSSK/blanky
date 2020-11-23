cdfdbeb79d3dd2b27cef86067437a7180fcc9e3e
TatsuyaSSK <ssktty514@gmail.com>
2020-11-23 14:36:52 +0900

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCl5Wm3xB84Ix78aRirIVo4QtMwIgmbDt0",
+    authDomain: "blanky-2fc41.firebaseapp.com",
+    databaseURL: "https://blanky-2fc41.firebaseio.com",
+    projectId: "blanky-2fc41",
+    storageBucket: "blanky-2fc41.appspot.com",
+    messagingSenderId: "423573406682",
+    appId: "1:423573406682:web:3875da92ba30922d30b6e3",
+    measurementId: "G-2WLY5S3DGS"
+  }
 };
