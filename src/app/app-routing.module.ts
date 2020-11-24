f7bba37dcf6fa9a5b8993fa31cc8a7f381dc3bde
TatsuyaSSK <ssktty514@gmail.com>
2020-11-24 13:10:14 +0900

    feat(privacy-component): プライバシーポリシー画面追加　#5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: 'terms',
     loadChildren: () => import('./terms/terms.module').then(m => m.TermsModule)
+  },
+  {
+    path: 'privacy',
+    loadChildren: () => import('./privacy/privacy.module').then(m => m.PrivacyModule)
   }
 ];
 
