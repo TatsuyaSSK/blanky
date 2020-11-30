02dbdf2efc91ddc54812115e1ce0ae326d0e9960
TatsuyaSSK <ssktty514@gmail.com>
2020-11-30 17:31:47 +0900

    feat(list.component): formの作成

--- src/app/list/list.module.ts
+++ src/app/list/list.module.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { ListRoutingModule } from './list-routing.module';
 import { ListComponent } from './list/list.component';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {TextFieldModule} from '@angular/cdk/text-field';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
   declarations: [ListComponent],
   imports: [
     CommonModule,
-    ListRoutingModule
+    ListRoutingModule,
+    MatButtonToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TextFieldModule,
+    MatFormFieldModule,
+    MatInputModule,
   ]
 })
 export class ListModule { }
--- src/app/list/list/list.component.ts
+++ src/app/list/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  form = this.fb.group({
+    keyward: ['', [
+      Validators.required,
+      Validators.maxLength(20)
+    ]],
+    sort: ['', [
+      Validators.required
+    ]],
+  });
+
+  submit(){
+    console.log(this.form.value)
+  }
+
+  get keywardControl(){
+    return this.form.get('keyward') as FormControl
+  }
+
+  get sortControl(){
+    return this.form.get('sort') as FormControl
+  }
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+
   }
 
 }
