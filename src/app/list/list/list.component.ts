427c3382a12a521cc91cca86595941e1d12d72bf
TatsuyaSSK <ssktty514@gmail.com>
2020-12-06 21:34:41 +0900

    feat(list.component): 条件なしを追加

--- src/app/list/list/list.component.ts
+++ src/app/list/list/list.component.ts
@@ -4,38 +4,33 @@ import { FormBuilder, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-
   sorts: any[] = [
-    {value: 'new', viewValue: '新着順'},
-    {value: 'accuracy', viewValue: '正答率順'},
+    { value: 'nothing', viewValue: '条件なし' },
+    { value: 'new', viewValue: '新着順' },
+    { value: 'accuracy', viewValue: '正答率順' },
   ];
 
   form = this.fb.group({
-    keyward: ['', [
-    ]],
-    sort: ['']
+    keyward: ['', []],
+    sort: [''],
   });
 
-  submit(){
+  submit() {
     console.log(this.form.value);
   }
 
-  get keywardControl(){
-    return this.form.get('keyward') as FormControl
+  get keywardControl() {
+    return this.form.get('keyward') as FormControl;
   }
 
-  get sortControl(){
-    return this.form.get('sort') as FormControl
+  get sortControl() {
+    return this.form.get('sort') as FormControl;
   }
 
-  constructor(
-    private fb: FormBuilder
-  ) { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
