3de130f3150e6f4ad991f550f8a1f0542fab5d2e
TatsuyaSSK <ssktty514@gmail.com>
2020-12-06 23:32:35 +0900

    feat(study.component): 学習画面作成　#11

--- src/app/study/study.module.ts
+++ src/app/study/study.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { StudyRoutingModule } from './study-routing.module';
 import { StudyComponent } from './study/study.component';
-
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [StudyComponent],
   imports: [
     CommonModule,
-    StudyRoutingModule
-  ]
+    StudyRoutingModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
 })
-export class StudyModule { }
+export class StudyModule {}
--- src/app/study/study/study.component.ts
+++ src/app/study/study/study.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-study',
   templateUrl: './study.component.html',
-  styleUrls: ['./study.component.scss']
+  styleUrls: ['./study.component.scss'],
 })
 export class StudyComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
+  display: boolean;
+  buttonValid: boolean;
 
-  constructor() { }
+  form = this.fb.group({
+    one: [''],
+    two: [''],
+    three: [''],
+    four: [''],
+  });
 
-  ngOnInit(): void {
+  checkAnswer() {
+    this.display = true;
+    this.buttonValid = true;
+    console.log(this.form.value);
   }
 
+  ngOnInit(): void {
+    this.display = false;
+    this.buttonValid = false;
+  }
 }
