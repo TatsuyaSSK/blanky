978c6cbdc4af5abe99a22bb31eafcab8b6b9d70b
TatsuyaSSK <ssktty514@gmail.com>
2020-12-07 23:37:40 +0900

    feat(shell.component):ヘッダー、フッターをshellに格納　#32

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,45 +1,53 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ShellComponent } from './shell/shell.component';
 
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    loadChildren: () => import('./top/top.module').then((m) => m.TopModule),
-  },
-  {
-    path: 'terms',
-    loadChildren: () =>
-      import('./terms/terms.module').then((m) => m.TermsModule),
-  },
-  {
-    path: 'create',
-    loadChildren: () =>
-      import('./create/create.module').then((m) => m.CreateModule),
-  },
-  {
-    path: 'privacy',
-    loadChildren: () =>
-      import('./privacy/privacy.module').then((m) => m.PrivacyModule),
-  },
-  {
-    path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListModule)
-  },
-  {
-    path: 'setting',
-    loadChildren: () =>
-      import('./setting/setting.module').then((m) => m.SettingModule),
-  },
-  {
-    path: 'study',
-    loadChildren: () =>
-      import('./study/study.module').then((m) => m.StudyModule),
-  },
-  {
-    path: '**',
-    component: NotFoundComponent,
+    component: ShellComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        loadChildren: () => import('./top/top.module').then((m) => m.TopModule),
+      },
+      {
+        path: 'terms',
+        loadChildren: () =>
+          import('./terms/terms.module').then((m) => m.TermsModule),
+      },
+      {
+        path: 'create',
+        loadChildren: () =>
+          import('./create/create.module').then((m) => m.CreateModule),
+      },
+      {
+        path: 'privacy',
+        loadChildren: () =>
+          import('./privacy/privacy.module').then((m) => m.PrivacyModule),
+      },
+      {
+        path: 'list',
+        loadChildren: () =>
+          import('./list/list.module').then((m) => m.ListModule),
+      },
+      {
+        path: 'setting',
+        loadChildren: () =>
+          import('./setting/setting.module').then((m) => m.SettingModule),
+      },
+      {
+        path: 'study',
+        loadChildren: () =>
+          import('./study/study.module').then((m) => m.StudyModule),
+      },
+      {
+        path: '**',
+        component: NotFoundComponent,
+      },
+    ],
   },
 ];
 
