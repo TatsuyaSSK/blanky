92305a5a8ce245fadd8c6bcd49181b388a9c50a1
TatsuyaSSK <ssktty514@gmail.com>
2021-02-03 00:05:25 +0900

    feat(user): 関数を分ける

---
+++ functions/src/payment.function.ts
@@ -0,0 +1,4 @@
+import * as functions from 'firebase-functions';
+import Stripe from 'stripe';
+import { stripe } from './utils/client';
+import { db } from './index';
--- functions/src/user.function.ts
+++ functions/src/user.function.ts
@@ -8,75 +8,19 @@ export const createUser = functions
   .region('asia-northeast1')
   .auth.user()
   .onCreate(async (user: auth.UserRecord) => {
-    // stripe顧客作成
-    const customer: Stripe.Customer = await stripe.customers.create({
-      name: user.displayName,
-      email: user.email,
-    });
-
-    // サンプルデータ作成
-    const problemId = db.collection('problems').doc().id;
-    const sampleData = await db
-      .doc(`problems/${user.uid}/random/${problemId}`)
-      .set({
-        problemId,
-        title: 'blankyへようこそ！',
-        englishText:
-          'blanky is a service that allows you to create your own original English questions just by uploading English sentences.',
-        japaneseText: '和訳文',
-        blankIndexes: [3, 5, 11, 14],
-        correctAnswerRate: 0,
-        createdAt: firestore.Timestamp.now(),
-        type: 'random',
-      });
-    console.log(sampleData);
-
-    // サンプルデータ作成→和訳文更新→algoliaにサンプルデータ作成
-
-    // userデータ作成
-    return db.doc(`users/${user.uid}`).set({
-      name: user.displayName,
-      avatarURL: user.photoURL,
-      email: user.email,
-      createdAt: new Date(),
-      stripeId: customer.id,
-      uid: user.uid,
-    });
+    const customer: Stripe.Customer = await createStripeCustomer(user);
+    await createSampleData(user);
+    await createUserData(user, customer);
   });
 
 export const deleteUser = functions
   .region('asia-northeast1')
   .auth.user()
   .onDelete(async (user) => {
-    // stripe顧客削除
-    let stripeId = '';
-    let problemId = '';
-    await db
-      .doc(`users/${user.uid}`)
-      .get()
-      .then((doc) => {
-        stripeId = doc.data()?.stripeId;
-        problemId = doc.data()?.problemId;
-      });
-    const deleteStripeUser = await stripe.customers.del(stripeId);
-    console.log(deleteStripeUser);
-
-    // userのプロフィール画像削除
-    const deleteUserImage = await bucket.deleteFiles({
-      prefix: `users/${user.uid}`,
-    });
-    console.log(deleteUserImage);
-
-    // userの問題削除
-    const deleteUserProblem = await db
-      .doc(`problems/${user.uid}/random/${problemId}`)
-      .delete();
-    console.log(`start to delete user data: ${deleteUserProblem}`);
-
-    // 問題削除→algoliaの問題データ削除
-
-    // userのデータ削除
-    return db.doc(`users/${user.uid}`).delete();
+    await deleteStripeCustomer(user);
+    await deleteUserImage(user);
+    await deleteUserProblems(user);
+    await deleteUserData(user);
   });
 
 export const updateStripeUser = functions
@@ -88,3 +32,64 @@ export const updateStripeUser = functions
       name: data.name,
     });
   });
+
+function createStripeCustomer(user: auth.UserRecord) {
+  return stripe.customers.create({
+    name: user.displayName,
+    email: user.email,
+  });
+}
+
+function createSampleData(user: auth.UserRecord) {
+  const problemId = db.collection('problems').doc().id;
+  const sampleData = db.doc(`problems/${user.uid}/random/${problemId}`).set({
+    problemId,
+    title: 'blankyへようこそ！',
+    englishText:
+      'blanky is a service that allows you to create your own original English questions just by uploading English sentences.',
+    japaneseText: '和訳文',
+    blankIndexes: [3, 5, 11, 14],
+    correctAnswerRate: 0,
+    createdAt: firestore.Timestamp.now(),
+    type: 'random',
+  });
+  console.log(sampleData);
+}
+
+function createUserData(user: auth.UserRecord, customer: Stripe.Customer) {
+  return db.doc(`users/${user.uid}`).set({
+    name: user.displayName,
+    avatarURL: user.photoURL,
+    email: user.email,
+    createdAt: new Date(),
+    stripeId: customer.id,
+    uid: user.uid,
+  });
+}
+
+function deleteStripeCustomer(user: auth.UserRecord) {
+  let stripeId = '';
+  return db
+    .doc(`users/${user.uid}`)
+    .get()
+    .then((doc) => {
+      stripeId = doc.data()?.stripeId;
+    })
+    .then(() => {
+      return stripe.customers.del(stripeId);
+    });
+}
+
+function deleteUserImage(user: auth.UserRecord) {
+  return bucket.deleteFiles({
+    prefix: `users/${user.uid}`,
+  });
+}
+
+function deleteUserProblems(user: auth.UserRecord) {
+  return db.doc(`problems/${user.uid}`).delete();
+}
+
+function deleteUserData(user: auth.UserRecord) {
+  return db.doc(`users/${user.uid}`).delete();
+}
