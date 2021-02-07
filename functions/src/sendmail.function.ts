3502a793c092226fc55e7e083f2c4924d41d1fce
TatsuyaSSK <ssktty514@gmail.com>
2021-02-07 20:38:35 +0900

    feat(sendmail): 課金スタート時、課金ストップ時にメール送信

--- functions/src/sendmail.function.ts
+++ functions/src/sendmail.function.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import { sendEmail } from './utils/sendgrid';
+import { db } from './index';
 
 export const welcomeEmail = functions
   .region('asia-northeast1')
@@ -16,5 +17,50 @@ export const welcomeEmail = functions
       });
     } else {
       return null;
+    }
+  });
+
+export const startSubscriptionEmail = functions
+  .region('asia-northeast1')
+  .firestore.document(`customers/{uid}/subscriptions/{subscriptionsId}`)
+  .onCreate(async (snap, context) => {
+    const data = snap.data();
+    if (data.status === 'active') {
+      await db
+        .doc(`users/${context.params.uid}`)
+        .get()
+        .then((user) => {
+          return sendEmail({
+            to: user.data()?.email,
+            templateId: 'd-31035badd7cc4654a4c059308e1eb894',
+            dynamicTemplateData: {
+              subject: 'プレミアムプランに登録しました',
+              plan: 'プレミアムプラン',
+            },
+          });
+        });
+    }
+  });
+
+export const stopSubscriptionEmail = functions
+  .region('asia-northeast1')
+  .firestore.document(`customers/{uid}/subscriptions/{subscriptionsId}`)
+  .onUpdate(async (snap, context) => {
+    const data = snap.after.data();
+    if (data.status !== 'canceled' && data.canceled_at !== 'null') {
+      await db
+        .doc(`users/${context.params.uid}`)
+        .get()
+        .then((user) => {
+          return sendEmail({
+            to: user.data()?.email,
+            templateId: 'd-2eaa7c53a9864ee88ccfc4fbc4799afe',
+            dynamicTemplateData: {
+              subject: 'プレミアムプランが解約されました',
+              plan: 'プレミアムプラン',
+              cancel_at: data.cancel_at,
+            },
+          });
+        });
     }
   });
