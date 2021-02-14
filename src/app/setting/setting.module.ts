66185a23153425d9ca83c2dd9950cc79f9bcca7d
TatsuyaSSK <ssktty514@gmail.com>
2021-02-15 00:19:10 +0900

    feat(setting): 各コンポーネントを読み込むよう修正

--- src/app/setting/setting.module.ts
+++ src/app/setting/setting.module.ts
@@ -13,9 +13,12 @@ import { CropComponent } from './crop/crop.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ProfileComponent } from './profile/profile.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
-  declarations: [SettingComponent, WithdrawalComponent, CropComponent],
+  declarations: [SettingComponent, WithdrawalComponent, CropComponent, ProfileComponent, PaymentComponent],
   imports: [
     CommonModule,
     SettingRoutingModule,
@@ -28,6 +31,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     ReactiveFormsModule,
     FormsModule,
     MatFormFieldModule,
+    MatTabsModule,
   ],
 })
 export class SettingModule {}
