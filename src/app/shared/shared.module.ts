3d792c09bfcae26ce9a819daad5ab521d6c16bd0
TatsuyaSSK <ssktty514@gmail.com>
2021-02-16 19:35:26 +0900

    feat(card): カード削除ボタン表示

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,17 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { CardsComponent } from './cards/cards.component';
 import { CardComponent } from './card/card.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [CardsComponent, CardComponent],
-  imports: [CommonModule, SharedRoutingModule],
-  exports: [CardsComponent],
+  imports: [
+    CommonModule,
+    SharedRoutingModule,
+    MatIconModule,
+    MatSnackBarModule,
+  ],
+  exports: [CardsComponent, MatIconModule],
 })
 export class SharedModule {}
