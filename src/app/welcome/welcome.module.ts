6b3edf92cafcb7c29085bd0417c0fbf3e83cf711
TatsuyaSSK <ssktty514@gmail.com>
2021-02-17 00:09:13 +0900

    feat(welcome): 3つのコンポーネント追加

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
-
+import { HeroComponent } from './hero/hero.component';
+import { UseComponent } from './use/use.component';
+import { PlansComponent } from './plans/plans.component';
 
 @NgModule({
-  declarations: [WelcomeComponent],
-  imports: [
-    CommonModule,
-    WelcomeRoutingModule
-  ]
+  declarations: [WelcomeComponent, HeroComponent, UseComponent, PlansComponent],
+  imports: [CommonModule, WelcomeRoutingModule],
 })
-export class WelcomeModule { }
+export class WelcomeModule {}
