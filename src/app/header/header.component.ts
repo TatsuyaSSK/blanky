bb69094917956e836b9fd2c6430db70a30062eaf
TatsuyaSSK <ssktty514@gmail.com>
2021-02-17 23:51:39 +0900

    feat(hero): ヒーロー画面作成

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { User } from '../interfaces/user';
 import { AuthService } from '../services/auth.service';
 import { StripeService } from '../services/stripe.service';
+import { SignupComponent } from '../welcome/signup/signup.component';
 
 @Component({
   selector: 'app-header',
@@ -15,7 +17,8 @@ export class HeaderComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private StripeService: StripeService
+    private StripeService: StripeService,
+    public dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
@@ -34,5 +37,9 @@ export class HeaderComponent implements OnInit {
 
   logout() {
     this.authService.logout();
+  }
+
+  openSignUpDialog() {
+    this.dialog.open(SignupComponent);
   }
 }
--- src/app/welcome/hero/hero.component.ts
+++ src/app/welcome/hero/hero.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { SignupComponent } from '../signup/signup.component';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openSignUpDialog() {
+    this.dialog.open(SignupComponent);
   }
-
 }
