e45a475f8e5542a57e7e649232278a5e33731a12
TatsuyaSSK <ssktty514@gmail.com>
2021-02-21 23:28:55 +0900

    commit

--- src/app/setting/withdrawal/withdrawal.component.ts
+++ src/app/setting/withdrawal/withdrawal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -7,11 +10,26 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./withdrawal.component.scss'],
 })
 export class WithdrawalComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private afn: AngularFireFunctions,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   withdrawal() {
-    this.authService.withdrawal();
+    const callable = this.afn.httpsCallable('deleteAfUser');
+    return callable(this.authService.uid)
+      .toPromise()
+      .then(() => {
+        this.router.navigateByUrl('/');
+        this.authService.afAuth.signOut().then(() => {
+          this.snackBar.open('退会しました', null, {
+            duration: 2000,
+          });
+        });
+      });
   }
 }
