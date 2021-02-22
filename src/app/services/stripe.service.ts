2738186362fb27f7f58d1887b073b72177e487cc
TatsuyaSSK <ssktty514@gmail.com>
2021-02-22 20:07:13 +0900

    import変更

--- src/app/services/stripe.service.ts
+++ src/app/services/stripe.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { loadStripe } from '@stripe/stripe-js';
 import { environment } from '../../environments/environment';
 import { AuthService } from './auth.service';
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root',
